f98112b2218b79b6909b11dd9137d46aadb95ddd
chLi <565353780@qq.com>
2026-01-22 10:11:33 +0800

    add missing package

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,4 +1,5 @@
 pip3 install torch torchvision torchaudio \
   --index-url https://download.pytorch.org/whl/cu124
 
-pip install tensorboard prefetch_generator open3d trimesh
+pip install tensorboard prefetch_generator open3d trimesh \
+  tqdm
